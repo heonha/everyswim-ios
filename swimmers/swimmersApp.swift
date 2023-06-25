5e4b4d54d76bbf3296c7ab6ccccf9cb5b18fd92e
HeonJin Ha <weizen511@gmail.com>
2023-06-25 18:17:25 +0900

    Initial Commit

---
+++ swimmers/swimmersApp.swift
@@ -0,0 +1,17 @@
+//
+//  swimmersApp.swift
+//  swimmers
+//
+//  Created by HeonJin Ha on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct swimmersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
