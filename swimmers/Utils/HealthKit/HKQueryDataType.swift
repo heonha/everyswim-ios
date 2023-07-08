984e5fcfbef386dbeed9652752f5315c93244729
HeonJin Ha <weizen511@gmail.com>
2023-07-08 16:18:46 +0900

    refactor: 건강 연동시 불필요한 권한 항목 제거

--- swimmers/Utils/HealthKit/HKQueryDataType.swift
+++ swimmers/Utils/HealthKit/HKQueryDataType.swift
@@ -8,33 +8,18 @@
 import HealthKit
 
 enum HKQueryDataType {
-    case kcal, stroke, distance, restKcal, userO2
-    case depth, waterTemp
+    case kcal, restKcal, stroke, distance
     
     func dataType() -> HKQuantityType? {
         switch self {
         case .kcal:
             return HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)
+        case .restKcal:
+            return HKObjectType.quantityType(forIdentifier: .basalEnergyBurned)
         case .stroke:
             return HKObjectType.quantityType(forIdentifier: .swimmingStrokeCount)
         case .distance:
             return HKObjectType.quantityType(forIdentifier: .distanceSwimming)
-        case .restKcal:
-            return HKObjectType.quantityType(forIdentifier: .basalEnergyBurned)
-        case .userO2:
-            return HKObjectType.quantityType(forIdentifier: .vo2Max)
-        case .depth:
-            if #available(iOS 16.0, *) {
-                return HKObjectType.quantityType(forIdentifier: .underwaterDepth)
-            } else {
-                return nil
-            }
-        case .waterTemp:
-            if #available(iOS 16.0, *) {
-                return HKObjectType.quantityType(forIdentifier: .waterTemperature)
-            } else {
-                return nil
-            }
         }
     }
 }
