9d28f40c8b8e2f416fb36d7ff5acae6f68dc316b
HeonJin Ha <weizen511@gmail.com>
2023-09-14 23:13:32 +0900

    fix: 컨플릭 문제 해결

---
+++ swimmers/Presentation/Main/Calendar/EventDatePickerContainer.swift
@@ -0,0 +1,29 @@
+//
+//  EventDatePickerContainer.swift
+//  swimmers
+//
+//  Created by HeonJin Ha on 8/16/23.
+//
+
+import SwiftUI
+
+struct EventDatePickerContainer: View {
+    
+    @ObservedObject private var viewModel = EventDatePickerViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                EventDatePicker(viewModel: _viewModel)
+                    .padding(.horizontal, 8)
+                
+                EventListView(viewModel: _viewModel)
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    EventDatePickerContainer()
+}
---
+++ swimmers/Presentation/Main/Calendar/EventList/EventListView.swift
@@ -0,0 +1,129 @@
+//
+//  EventListView.swift
+//  swimmers
+//
+//  Created by HeonJin Ha on 8/16/23.
+//
+
+import SwiftUI
+
+struct EventListView: View {
+    
+    @ObservedObject private var viewModel: EventDatePickerViewModel
+    private var tintColor = AppColor.primary
+    
+    init(viewModel: ObservedObject<EventDatePickerViewModel>) {
+        self._viewModel = viewModel
+    }
+    
+}
+
+extension EventListView {
+    
+    var body: some View {
+        eventListContainer()
+            .padding(.horizontal)
+            .background(AppColor.skyBackground)
+            .frame(maxHeight: .infinity)
+    }
+    
+    // MARK: - Event Cells
+    private func eventListContainer() -> some View {
+        VStack {
+            eventListHeader()
+                .padding(.top)
+            
+            eventCellsContainer()
+            
+            Spacer()
+        }
+    }
+    
+    private func eventListHeader() -> some View {
+        HStack(spacing: 16) {
+            Text("수영 기록")
+                .font(.custom(.sfProMedium, size: 20))
+            
+            Spacer()
+            
+            Button {
+                viewModel.isMonthlyRecord.toggle()
+            } label: {
+                HStack(spacing: 2) {
+                    Image(systemName: viewModel.isMonthlyRecord ? "checkmark.circle.fill": "circle" )
+                    
+                    Text("월간 기록보기")
+                        .font(.custom(.sfProLight, size: 16))
+                }
+            }
+            .tint(Color.init(uiColor: .label))
+        }
+    }
+    
+    private func eventCellsContainer() -> some View {
+        Group {
+            ScrollView(showsIndicators: false) {
+                if !viewModel.presentedEventData.isEmpty {
+                    dailyAndMonthlySwitcher()
+                } else {
+                    eventCellPlaceholder()
+                }
+            }
+        }
+    }
+    
+    private func dailyAndMonthlySwitcher() -> some View {
+        Group {
+            if viewModel.isMonthlyRecord {
+                monthlyEventList(viewModel.presentedEventData)
+            } else {
+                let dayMetadata = viewModel.extractFirstEvent(date: viewModel.currentDate)
+                if let dayMetadata = dayMetadata {
+                    dailyEventList(dayMetadata)
+                } else {
+                    eventCellPlaceholder()
+                }
+            }
+        }
+    }
+    
+    private func dailyEventList(_ workout: DatePickerEventData) -> some View {
+        ForEach(workout.event) { workout in
+            EventListCell(data: workout)
+        }
+    }
+    
+    private func monthlyEventList(_ workouts: [DatePickerEventData]) -> some View {
+        ForEach(workouts) { workout in
+            ForEach(workout.event) { event in
+                EventListCell(data: event)
+            }
+        }
+    }
+    
+    private func eventCellPlaceholder() -> some View {
+        VStack(alignment: .center) {
+            Spacer()
+            
+            Text("수영 기록이 없습니다.")
+                .font(.custom(.sfProMedium, size: 19))
+                .foregroundColor(.gray)
+            
+            Spacer()
+        }
+        .frame(minHeight: 200, idealHeight: 300)
+    }
+    
+}
+
+#if DEBUG
+struct EventListView_Previews: PreviewProvider {
+    
+    static var viewModel = ObservedObject<EventDatePickerViewModel>.init(initialValue: EventDatePickerViewModel())
+    
+    static var previews: some View {
+        // EventListView(viewModel: viewModel)
+        EventDatePickerContainer()
+    }
+}
+#endif
