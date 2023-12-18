8c49b169aae9cfe19a7f6654ad9dffaa11457244
HeonJin Ha <weizen511@gmail.com>
2023-12-18 18:01:51 +0900

    fix: 카카오 키워드로 장소검색 API 테스트 코드 개선

--- everyswim-tests/Map/KakaoPlaceManagerTests.swift
+++ everyswim-tests/Map/KakaoPlaceManagerTests.swift
@@ -28,24 +28,23 @@ class KakaoPlaceManagerTests: XCTestCase {
     }
     
     func test_fetch_place_list() {
-        // Given
         let expectation = expectation(description: "카카오 키워드 검색 수행")
-
-        // Then
-        sut.$places
-            .receive(on: DispatchQueue.global())
-            .filter { !$0.isEmpty }
-            .sink { locations in
-                print(locations)
-                expectation.fulfill()
-            }
-            .store(in: &cancellables)
         
-        // When
-        sut.findPlaceFromKeyword(query: "광명시수영장", coordinator: .init(latitude: 37.480667, longitude: 126.850454))
+        sut.findPlaceFromKeyword(query: "광명시수영장", 
+                                 coordinator: .init(latitude: 37.480667, longitude: 126.850454)) { result in
+            switch result {
+            case .success(let places):
+                if !places.isEmpty {
+                    expectation.fulfill()
+                } else {
+                    XCTFail("장소 데이터가 비어있습니다.")
+                }
+            case .failure(let error):
+                XCTFail("오류가 발생했습니다. \(error.localizedDescription)")
+            }
+        }
         
         waitForExpectations(timeout: 5, handler: nil)
-        
     }
     
 }
